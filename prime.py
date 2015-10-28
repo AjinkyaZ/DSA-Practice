cf909387489b4379b93015bce47eb25883e632f3
AjinkyaZ <ajinkya.zadbuke@gmail.com>
2015-10-28 11:15:23 +0530

    fixed prime.py

--- prime.py
+++ prime.py
@@ -2,9 +2,11 @@ from math import sqrt
 def is_prime(n):
     upperlim =  int(sqrt(n))
     if n == 1: return "is not a prime"
+    if n == 0: return "is neither prime nor composite"
     for i in xrange(2, upperlim + 1):
         if i > 1 and n % i == 0:
             return "is not a prime"
     return "is a prime"
-#n = input("enter number\n")
-#print n, is_prime(n)
+
+n = input("enter number\n")
+print n, is_prime(n)

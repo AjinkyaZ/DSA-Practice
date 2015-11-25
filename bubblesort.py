92e1afb502660d202f9c2e7c03142a45249a69ec
AjinkyaZ <ajinkya.zadbuke@gmail.com>
2015-11-25 19:26:53 +0530

    fixed bubblesort

--- bubblesort.py
+++ bubblesort.py
@@ -1,10 +1,11 @@
 def bubble_sort(a_list):
 	len_a = len(a_list)
-	for p_num in range(len_a - 1, 0, -1):
-		for i in range(p_num):
-			temp = a_list[i]
-			a_list[i] = a_list[i+1]
-			a_list[i+1] = temp
+	for i in range(len_a-1, 0, -1):
+		for j in range(i):
+			if(a_list[j]>a_list[j+1]):
+				temp = a_list[j]
+				a_list[j] = a_list[j+1]
+				a_list[j+1] = temp
 	return a_list
 
 a = input("enter list of numbers\n")

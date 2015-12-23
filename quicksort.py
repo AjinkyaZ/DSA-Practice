d3c8c623c1e198b637c9b7a3b72db91ce7b9c304
AjinkyaZ <ajinkya.zadbuke@gmail.com>
2015-12-23 13:24:56 +0530

    rewrote quicksort

--- quicksort.py
+++ quicksort.py
@@ -1,41 +1,43 @@
 import random
 
+
 def qsort(a):
-    if a == []: 
-        return []
-    else:
-        pivot = a[0]
-        lesser, equal, greater = partition(a[1:], [], [pivot], [])
-        return qsort(lesser) + equal + qsort(greater)
-
-def partition(a, l, e, g):
-    while a != []:
-        head = a.pop(0)
-        if head < e[0]:
-            l = [head] + l
-        elif head > e[0]:
-            g = [head] + g
-        else:
-            e = [head] + e
-    return (l, e, g)
+    qsort_args(a, 0, len(a) - 1)
 
 
-#not in-place
+def qsort_args(a, start, end):
+    if start < end:
+        pivot = partition(a, start, end)
+        qsort_args(a, start, pivot - 1)
+        qsort_args(a, pivot + 1, end)
+
+
+def partition(a, start, end):
+    pivot = start + random.randrange(end - start + 1)
+    a[pivot], a[end] = a[end], a[pivot]
+    for i in range(start, end):
+        if a[i] <= a[end]:
+            a[i], a[start] = a[start], a[i]
+            start += 1
+    a[start], a[end] = a[end], a[start]
+    return start
+
+
 def qsort_alt(A):
-    ln =len(A)
+    ln = len(A)
     less = []
     equal = []
     greater = []
 
     if ln > 1:
-        #pivot = A[0]
-        #pivot = A[ln/2]
-        pivot = A[random.randint(0,ln-1)]
-        #print "pivot", pivot
+            #pivot = A[0]
+            #pivot = A[ln/2]
+        pivot = A[random.randint(0, ln-1)]
+        # print "pivot", pivot
         for x in A:
             lesser = qsort_alt([x for x in A if x < pivot])
             equal = [x for x in A if x == pivot]
-            greater = qsort_alt([ x for x in A if x > pivot])
+            greater = qsort_alt([x for x in A if x > pivot])
             """if x < pivot:
                 lesser.append(x)
                 #print "lesser --> ", less
@@ -45,18 +47,20 @@ def qsort_alt(A):
             if x > pivot:
                 greater.append(x)
                 #print "greater --> ", greater"""
-        #print " "
+        # print " "
         return lesser+equal+greater
     else:
         return A
 
+
 def main():
-    a = raw_input("enter array\n").split(' ')
-    a = [int(i) for i in a]
-    print "original array :\n", a
-    print "sorted array (qsort_alt):\n", qsort_alt(a)
+    arr = raw_input("enter array\n").split(' ')
+    arr = [int(i) for i in arr]
+    print "original array :\n", arr
+    print "sorted array (qsort_alt):\n", qsort_alt(arr)
     print ""
-    print "sorted array (qsort):\n", qsort(a)
+    qsort(arr)
+    print "sorted array (qsort):\n", arr
 
 if __name__ == "__main__":
     main()

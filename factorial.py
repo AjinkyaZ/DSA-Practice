59f3ed89a830b1dd2806ce690db9c1b6cfb5d54d
ajinkya.z <ajinkya.z@edgenetworks.in>
2017-01-11 17:03:44 +0530

    cleaning up

--- factorial.py
+++
@@ -1,8 +0,0 @@
-def factorial(n):
-    fact = 1
-    if n<=1:
-        return f
-    else:
-        for i in range(1,n+1):
-            fact = fact*i
-        return fact
--- floydtriangle.py
+++
@@ -1,7 +0,0 @@
-n = input("Enter the number of rows \n")
-x = 1
-for i in range(1, n+1):
-        for j in range(1, i+1):
-           print x,
-           x = x+1
-        print "\n"
